9d9c53364415396b5e55615c3dd19d0692c65f9d
Asheesh <asheesh.choksi@gmail.com>
2024-12-31 09:49:08 +0000

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Hospital Management",
+    "summary": "Manage Patient Appointment Workflow", 
+    "author": "We",
+    "license": "AGPL-3",
+    "category": "Uncategorized",
+    "website": "https://github.com/datalytiks",
+    "version": "18.0.1.0",
+    "depends": ["base"],
+    "application": True,
+    "installable": True,
+}
+
---
+++ models/patient.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+class HospitalPatient(models.Model):
+    _name = 'hospital.patient'
+    _description = 'Patient Record'
+
+    
+    patient_name = fields.Char(string='Name', required=True)
+    patient_age = fields.Integer(string='Age')
+    notes = fields.Text(string="Notes")
+    image = fields.Binary(string="Image")
+
